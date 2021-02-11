6eabe310e58ef6a9f7d798571cabc96a3a0b5393
ThreedTX <ReedWinstonNorton@gmail.com>
2021-02-11 12:44:13 -0600

    Almost Finished withh phase1

---
+++ app/shipping_form.py
@@ -0,0 +1,17 @@
+from wtforms import FlaskForm
+from wtforms.fields import (
+    StringField, SelectField, BooleanField, SubmitField)
+from wtforms.validators import DataRequired
+from map.map import map
+
+
+v = [DataRequired()]
+
+
+class ShippingForm(FlaskForm):
+    name_sender = StringField("Sender name", v)
+    name_recipient = StringField("Recipient name", v)
+    origin = SelectField("Origin", v, choices=map)
+    destination = SelectField("Destination", v, choices=map)
+    express_shipping = BooleanField("Express shipping", v)
+    submit = SubmitField("Submit")
--- package_tracker.py
+++ package_tracker.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, Blueprint
 # import app.routes as routes
 # import app.api_routes as api_routes
-from .config import Config
-from .models import db
+from app.config import Config
+# from .models import db
 
 bp = Blueprint("root", __name__, url_prefix="")
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@bp.route("/")
+
+@app.route("/")
 def root():
     return render_template("root.html")
+
+
+@app.route("/new_package")
+def new_package():
+    return render_template('shipping_request.html')
+
