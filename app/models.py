f9a07c2ae3171962b41cbd57996c43feeb50180c
Max Lettenberger <maxlettenberger@gmail.com>
2021-02-11 15:38:35 -0600

    Working on first migration

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Package(db.Model):
+    __tablename__ = 'packages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    recipient = db.Column(db.String(20), nullable=False)
+    origin = db.Column(db.String(20), nullable=False)
+    destination = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(20), nullable=False)
+
+    
--- package_tracker.py
+++ package_tracker.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, Blueprint, redirect
+from flask_migrate import Migrate
 # import app.routes as routes
 # import app.api_routes as api_routes
 from app.config import Config
 # from .models import db
 from app.shipping_form import ShippingForm
 
-bp = Blueprint("root", __name__, url_prefix="")
-
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
@@ -19,9 +20,7 @@ def root():
 @app.route("/new_package", methods=["GET", "POST"])
 def new_package():
     form = ShippingForm()
-    print("*****************22***************")
     if form.validate_on_submit():
         # form.populate_obj(order)
-        print("***************25*****************")
         return redirect("/")
     return render_template('shipping_request.html', form=form)
