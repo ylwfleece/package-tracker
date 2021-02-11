bfe76f1fc9b57fca48e6348d1b6ffa9e0d6e5d7e
Max Lettenberger <maxlettenberger@gmail.com>
2021-02-11 16:37:56 -0600

    Can save package in db

--- package_tracker.py
+++ package_tracker.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, Blueprint, redirect
 from flask_migrate import Migrate
-# import app.routes as routes
-# import app.api_routes as api_routes
 from app.config import Config
-# from .models import db
+from app.models import db, Package 
 from app.shipping_form import ShippingForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
-
 
 @app.route("/")
 def root():
@@ -21,6 +18,9 @@ def root():
 def new_package():
     form = ShippingForm()
     if form.validate_on_submit():
-        # form.populate_obj(order)
+        data = form.data
+        new_package = Package(name=data["name_sender"], recipient=data["name_recipient"], origin=data["origin"], destination=data["destination"], location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
         return redirect("/")
     return render_template('shipping_request.html', form=form)
